971bbeef128f0c78832034736d1a069839158789
yoyo232323 <38825063+yoyo232323@users.noreply.github.com>
2022-10-03 17:07:55 -0400

    Update ffmpeg_utils.py

--- bot/helper/ffmpeg_utils.py
+++ bot/helper/ffmpeg_utils.py
@@ -33,10 +33,10 @@ def encode(filepath):
             return None
         else:
             # Copy stream to hvc1
-            video_opts = '-c:v libaom-av1 -s 854x480  -sn -row-mt true -threads 8 -crf 28 -tile-columns 1 -tile-rows 0'
+            video_opts = '-c:v libx265 -crf 28 -s 1024x576 -sn -threads 8'
     else:
         # Transcode to h265 / hvc1
-        video_opts = '-c:v libaom-av1 -s 854x480  -sn'
+        video_opts = '-c:v libx265 -crf 28 -s 1024x576 -sn -threads 8'
     # Get the audio channel codec
     audio_codec = get_codec(filepath, channel='a:0')
     if audio_codec == []:
